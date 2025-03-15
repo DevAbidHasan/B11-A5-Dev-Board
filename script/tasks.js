171d862da1df9397ff0848156bcfa09d71f0d14e
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-03-15 10:33:02 +0600

    completed the project

--- script/tasks.js
+++ script/tasks.js
@@ -9,6 +9,10 @@ const assignedTaskCounter=parseInt(document.getElementById("assigned-task-counte
     document.getElementById("assigned-task-counter").innerText=assignedSum;
     // document.getElementById("ShopEase-btn").setAttribute=("disabled", true);
     alert("Board Updated Successfully.")
+    if(assignedSum===0)
+    {
+        alert("Congrates!!! You have completed all the current tasks.")
+    }
     const btnDisable=document.getElementById("ShopEase-btn");
     document.getElementById("ShopEase-btn").innerText="Completed"
     document.getElementById("ShopEase-btn").style.backgroundColor="gray";
@@ -26,6 +30,10 @@ const assignedTaskCounter=parseInt(document.getElementById("assigned-task-counte
     document.getElementById("header-task-counter").innerText=headerSum;
     document.getElementById("assigned-task-counter").innerText=assignedSum;
     alert("Board Updated Successfully.")
+    if(assignedSum===0)
+        {
+            alert("Congrates!!! You have completed all the current tasks.")
+        }
     const btnDisable=document.getElementById("cloud-btn");
     document.getElementById("cloud-btn").innerText="Completed"
     document.getElementById("cloud-btn").style.backgroundColor="gray";
@@ -43,6 +51,10 @@ const assignedTaskCounter=parseInt(document.getElementById("assigned-task-counte
     document.getElementById("header-task-counter").innerText=headerSum;
     document.getElementById("assigned-task-counter").innerText=assignedSum;
     alert("Board Updated Successfully.")
+    if(assignedSum===0)
+        {
+            alert("Congrates!!! You have completed all the current tasks.")
+        }
     const btnDisable=document.getElementById("swiftpay-btn");
     document.getElementById("swiftpay-btn").innerText="Completed"
     document.getElementById("swiftpay-btn").style.backgroundColor="gray";
@@ -60,6 +72,10 @@ const assignedTaskCounter=parseInt(document.getElementById("assigned-task-counte
     document.getElementById("header-task-counter").innerText=headerSum;
     document.getElementById("assigned-task-counter").innerText=assignedSum;
     alert("Board Updated Successfully.")
+    if(assignedSum===0)
+        {
+            alert("Congrates!!! You have completed all the current tasks.")
+        }
     const btnDisable=document.getElementById("meta-btn");
     document.getElementById("meta-btn").innerText="Completed"
     document.getElementById("meta-btn").style.backgroundColor="gray";
@@ -76,7 +92,11 @@ const assignedTaskCounter=parseInt(document.getElementById("assigned-task-counte
     const assignedSum=assignedTaskCounter-1;
     document.getElementById("header-task-counter").innerText=headerSum;
     document.getElementById("assigned-task-counter").innerText=assignedSum;
-    alert("Board Updated Successfully.")
+    alert("Board Updated Successfully.");
+    if(assignedSum===0)
+        {
+            alert("Congrates!!! You have completed all the current tasks.")
+        }
     const btnDisable=document.getElementById("google-btn");
     document.getElementById("google-btn").innerText="Completed"
     document.getElementById("google-btn").style.backgroundColor="gray";
@@ -93,7 +113,11 @@ const assignedTaskCounter=parseInt(document.getElementById("assigned-task-counte
     const assignedSum=assignedTaskCounter-1;
     document.getElementById("header-task-counter").innerText=headerSum;
     document.getElementById("assigned-task-counter").innerText=assignedSum;
-    alert("Board Updated Successfully.")
+    alert("Board Updated Successfully.");
+    if(assignedSum===0)
+        {
+            alert("Congrates!!! You have completed all the current tasks.")
+        }
     const btnDisable=document.getElementById("Glassdoar-btn");
     document.getElementById("Glassdoar-btn").innerText="Completed"
     document.getElementById("Glassdoar-btn").style.backgroundColor="gray";
